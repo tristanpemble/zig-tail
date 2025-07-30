696c7e8f5ee1272f1e28d62cd19c4bbb8194e109
Tristan Pemble <tpemble@gmail.com>
2025-07-29 18:15:16 -0700

    minimalize and support sized tail fields

--- src/root.zig
+++ src/root.zig
@@ -1,10 +1,51 @@
+test {
+    const Packet = extern struct {
+        host_len: usize,
+        tail: Tail(@This(), struct {
+            host: TailSlice(u8, .host_len),
+            buf_lens: usize,
+            read_buf: TailSlice(u8, .buf_lens),
+            write_buf: TailSlice(u8, .buf_lens),
+        }),
+    };
+
+    var buffer: [128]u8 align(@alignOf(Packet)) = @splat(0);
+
+    var stream = std.io.fixedBufferStream(&buffer);
+    var w = stream.writer();
+    try w.writeInt(usize, "hello world".len, native);
+    try w.writeAll("hello world");
+    try stream.seekBy(5); // alignment
+    try w.writeInt(usize, 16, native);
+
+    const packet: *Packet = @ptrCast(@alignCast(&buffer));
+    try std.testing.expectEqualSlices(u8, "hello world", packet.tail.get(.host));
+    try std.testing.expectEqual(16, packet.tail.get(.read_buf).len);
+    try std.testing.expectEqual(16, packet.tail.get(.write_buf).len);
+}
+
 pub fn Tail(Parent: type, Layout: type) type {
     return struct {
         const IsTail = {};
-        const tail_field_name = getTailField(Parent).?.name;
+        const tail_field_name = blk: {
+            var field: ?[]const u8 = null;
+            var err: []const u8 = "";
+            for (@typeInfo(Parent).@"struct".fields) |f| {
+                if (@typeInfo(f.type) != .@"struct" or !@hasDecl(f.type, "IsTail")) continue;
+                if (field != null) {
+                    if (err.len == 0) {
+                        err = "multiple tail types on " ++ @typeName(Parent) ++ ", found: " ++ field.?;
+                    }
+                    err = err ++ ", " ++ f.name;
+                }
+                field = f.name;
+            }
+            if (err.len > 0) @compileError(err);
+            break :blk field.?;
+        };
 
-        pub fn get(self: *@This(), comptime field: FieldEnum(Layout)) PtrTypeOf(@tagName(field)) {
-            const parent: *Parent = @fieldParentPtr(tail_field_name, self);
+        pub fn get(self: *@This(), comptime field: FieldEnum(Layout)) []@FieldType(Layout, @tagName(field)).Element {
+            const parent: *Parent = @alignCast(@fieldParentPtr(tail_field_name, self));
             const bytes: [*]align(@alignOf(Parent)) u8 = @ptrCast(parent);
             const offset = offsetOf(parent, @tagName(field));
             const size = sizeOf(parent, @tagName(field));
@@ -12,82 +53,49 @@ pub fn Tail(Parent: type, Layout: type) type {
             return @ptrCast(@alignCast(bytes[offset..][0..size]));
         }
 
-        pub fn copy(self: *@This(), comptime field: FieldEnum(Layout), value: []const ElementOf(@tagName(field))) void {
+        pub fn copy(self: *@This(), comptime field: FieldEnum(Layout), value: []const @FieldType(Layout, @tagName(field)).Element) void {
             const dest = self.get(field);
 
             @memcpy(dest, value);
         }
 
-        pub fn getSize(self: *@This()) usize {
-            const parent: *Parent = @fieldParentPtr(tail_field_name, self);
-            var size: usize = 0;
-            inline for (@typeInfo(Layout).@"struct".fields) |f| {
-                size += sizeOf(parent, f.name);
-            }
-            return size;
+        fn LenOf(comptime field_name: []const u8) type {
+            const len_field = @tagName(@FieldType(Layout, field_name).len_field);
+            return if (comptime @hasField(Parent, len_field))
+                @FieldType(Parent, len_field)
+            else if (comptime @hasField(Layout, len_field))
+                @FieldType(Layout, len_field)
+            else
+                @compileError("'" ++ len_field ++ "', the length field of '" ++ field_name ++ "', does not exist on either '" ++ @typeName(Parent) ++ "' or the layout struct.");
         }
 
-        fn PtrTypeOf(field_name: []const u8) type {
-            if (isTailSlice(@FieldType(Layout, field_name))) {
-                const Element = ElementOf(field_name);
-                if (hasTailField(Element)) {
-                    return *@FieldType(Layout, field_name);
-                } else {
-                    return []Element;
-                }
+        fn lenOf(parent: *const Parent, comptime field_name: []const u8) LenOf(field_name) {
+            const len_field = @tagName(@FieldType(Layout, field_name).len_field);
+            return if (comptime @hasField(Parent, len_field))
+                @field(parent, len_field)
+            else if (comptime @hasField(Layout, len_field)) blk: {
+                const offset = offsetOf(parent, len_field);
+                const ptr: *LenOf(field_name) = @ptrFromInt(@intFromPtr(parent) + offset);
+                break :blk ptr.*;
+            } else @compileError("'" ++ len_field ++ "', the length field of '" ++ field_name ++ "', does not exist on either '" ++ @typeName(Parent) ++ "' or the layout struct.");
+        }
+
+        fn sizeOf(parent: *const Parent, comptime field_name: []const u8) usize {
+            const Field = @FieldType(Layout, field_name);
+            if (comptime isTailSlice(Field)) {
+                const len = lenOf(parent, field_name);
+                return @sizeOf(Field.Element) * len;
             } else {
-                return *@FieldType(Layout, field_name);
+                return @sizeOf(Field);
             }
         }
 
-        fn ElementOf(field_name: []const u8) type {
-            const config = @FieldType(Layout, field_name);
-            return config.Element;
-        }
-
-        fn LenTypeOf(field_name: []const u8) type {
-            return @FieldType(Parent, lenFieldOf(field_name));
-        }
-
-        fn lenFieldOf(field_name: []const u8) []const u8 {
-            const config = @FieldType(Layout, field_name);
-            return @tagName(config.len.?);
-        }
-
-        fn sizeFieldOf(field_name: []const u8) []const u8 {
-            const config = @FieldType(Layout, field_name);
-            return @tagName(config.size);
-        }
-
-        fn lenOf(parent: *Parent, comptime field_name: []const u8) LenTypeOf(field_name) {
-            return @field(parent, lenFieldOf(field_name));
-        }
-
-        fn sizeOf(parent: *Parent, comptime field_name: []const u8) usize {
-            const T = ElementOf(field_name);
-            const len = lenOf(parent, field_name);
-            if (comptime hasTailField(T)) {
-                // todo refactor..
-                const offset = offsetOf(parent, field_name);
-                var ptr: *T = @ptrFromInt(@intFromPtr(parent) + offset);
-
-                var size: usize = 0;
-                for (0..len) |_| {
-                    const n = @sizeOf(T) + @field(ptr, getTailField(T).?.name).getSize();
-                    size += n;
-                    ptr = @ptrFromInt(@intFromPtr(ptr) + n);
-                }
-                return size;
-            }
-            return len * @sizeOf(T);
-        }
-
-        fn offsetOf(parent: *Parent, comptime field_name: []const u8) usize {
+        fn offsetOf(parent: *const Parent, comptime field_name: []const u8) usize {
             var offset: usize = @sizeOf(Parent);
             inline for (@typeInfo(Layout).@"struct".fields) |f| {
-                const T = ElementOf(f.name);
+                const T = if (comptime isTailSlice(f.type)) f.type.Element else f.type;
                 offset = std.mem.alignForward(usize, offset, @alignOf(T));
-                if (std.mem.eql(u8, f.name, field_name)) {
+                if (comptime std.mem.eql(u8, f.name, field_name)) {
                     return offset;
                 }
                 offset += sizeOf(parent, f.name);
@@ -97,135 +105,24 @@ pub fn Tail(Parent: type, Layout: type) type {
     };
 }
 
-pub fn getTailField(comptime T: type) ?StructField {
-    if (@typeInfo(T) != .@"struct") return null;
-    var field: ?StructField = null;
-    var err: []const u8 = "";
-    for (@typeInfo(T).@"struct".fields) |f| {
-        if (!isTail(f.type)) continue;
-        if (field != null) {
-            if (err.len == 0) {
-                err = "multiple tail types on " ++ @typeName(T) ++ ", found: ";
-            } else {
-                err = err ++ ", ";
-            }
-            err = err ++ f.name;
-        }
-        field = f;
-    }
-    if (err.len > 0) @compileError(err);
-    return field;
-}
-
-pub fn isTail(comptime T: type) bool {
-    return @typeInfo(T) == .@"struct" and @hasDecl(T, "IsTail");
-}
-
-pub fn hasTailField(comptime T: type) bool {
-    return getTailField(T) != null;
-}
-
-pub fn SizedSlice(T: type, len_field: ?@Type(.enum_literal)) type {
+pub fn TailSlice(T: type, field: @Type(.enum_literal)) type {
     return struct {
-        comptime {
-            if (hasTailField(T)) {}
-        }
-        pub const IsTailSlice = {};
-        pub const Element = T;
-        pub const len = len_field;
-    };
-}
+        const is_tail_slice = {};
 
-pub fn UnsizedSlice(T: type, len_field: ?@Type(.enum_literal), size_field: @Type(.enum_literal)) type {
-    return struct {
-        pub const IsTailSlice = {};
         pub const Element = T;
-        pub const len = len_field;
-        pub const size = size_field;
+        pub const len_field = field;
     };
 }
 
 pub fn isTailSlice(comptime T: type) bool {
-    return @typeInfo(T) == .@"struct" and @hasDecl(T, "IsTailSlice");
+    return @typeInfo(T) == .@"struct" and
+        @hasDecl(T, "is_tail_slice");
 }
 
-test {
-    const Packet = extern struct {
-        host_len: u8 = 0,
-        buf_lens: u8 = 0,
-        tail: Tail(@This(), struct {
-            host: SizedSlice(u8, .host_len),
-            read_buf: SizedSlice(u8, .buf_lens),
-            write_buf: SizedSlice(u8, .buf_lens),
-        }),
-    };
-
-    const host = "foobar";
-
-    var buffer: [100]u8 align(@alignOf(Packet)) = undefined;
-    buffer[0] = @intCast(host.len);
-    buffer[1] = 16;
-
-    const packet: *Packet = @alignCast(@ptrCast(&buffer));
-    packet.tail.copy(.host, @ptrCast(host[0..]));
-
-    try std.testing.expectEqualSlices(u8, host[0..], packet.tail.get(.host));
-    try std.testing.expectEqual(16, packet.tail.get(.read_buf).len);
-    try std.testing.expectEqual(16, packet.tail.get(.write_buf).len);
-}
-
-test "nested" {
-    const Page = extern struct {
-        len: u8,
-        tail: Tail(@This(), struct {
-            chars: SizedSlice(u8, null),
-        }),
-    };
-    const Database = extern struct {
-        page_count: u8,
-        page_size: u8,
-        tail: Tail(@This(), struct {
-            strings: UnsizedSlice(Page, .page_count, .page_size),
-        }),
-    };
-
-    try std.testing.expect(true);
-
-    const strings = [_][]const u8{
-        "small",
-        "a bigger string",
-        "an even bigger string",
-        "someone stop this man before he makes a bigger string",
-    };
-
-    var buffer: [100]u8 align(@alignOf(Database)) = undefined;
-    var stream = std.io.fixedBufferStream(&buffer);
-    var w = stream.writer();
-    try w.writeInt(u8, 4, .little);
-    try w.writeInt(u8, strings[0].len, .little);
-    try w.writeAll(strings[0]);
-    try w.writeInt(u8, strings[1].len, .little);
-    try w.writeAll(strings[1]);
-    try w.writeInt(u8, strings[2].len, .little);
-    try w.writeAll(strings[2]);
-    try w.writeInt(u8, strings[3].len, .little);
-
-    const string_list: *Database = @alignCast(@ptrCast(&buffer));
-
-    try std.testing.expectEqual(
-        4 + strings[0].len + strings[1].len + strings[2].len + strings[3].len,
-        string_list.tail.getSize(),
-    );
-    // for (0..strings.len) |i| {
-    //     try std.testing.expectEqualSlices(
-    //         u8,
-    //         strings[i],
-    //         string_list.tail.get(.strings).get(i).tail.get(.chars),
-    //     );
-    // }
-}
+const native = builtin.cpu.arch.endian();
 
 const std = @import("std");
 const Allocator = std.mem.Allocator;
 const FieldEnum = std.meta.FieldEnum;
-const StructField = std.builtin.Type.StructField;
+const FieldInfo = std.builtin.Type.FieldInfo;
+const builtin = @import("builtin");
